c3d4a70746940ec9da1235eb0b86f249c85cb4dc
Sebastian Szelest <seba3444gaming@gmail.com>
2022-06-01 21:07:33 +0200

    Lab3

--- spaApp/resources/js/app.js
+++ spaApp/resources/js/app.js
@@ -4,9 +4,15 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+import Vue from 'vue';
+import Spa from './Spa.vue';
+import router from './router';
 
 window.Vue = require('vue');
+
+require('./bootstrap');
+
+
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +25,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/Navigation.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +35,10 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    template:'<spa/>',
+    components: {
+        Spa
+    },
+    router
 });
+
--- spaApp/routes/web.php
+++ spaApp/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', function ($any) {
+Route::get('/{any}', function () {
     return view('spa');
 })->where('any','.*');
